821813003059c56ceeb7c2174bf84e9c0fa57896
darnissan <67154616+darnissan@users.noreply.github.com>
2023-05-11 17:38:06 +0300

    basic c-a interface

--- Nice_text_assessment.py
+++ Nice_text_assessment.py
@@ -1,10 +1,9 @@
-#pip install nrclex
+# pip install nrclex
+from nrclex import NRCLex
 import nltk
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
-
-from nrclex import NRCLex
 
 
 def analyze_emotions(text):
@@ -13,7 +12,7 @@ def analyze_emotions(text):
 
 
 # Example usage
-text = Hi, I'm having a problem with my computer. The screen is completely blank and I can't see anything. Can you help me?
+text = "Hi, I'm having a problem with my computer. The screen is completely blank and I can't see anything. Can you help me?"
 emotion = analyze_emotions(text)
 
 # Print the emotion and its score
@@ -21,3 +20,15 @@ for e, score in emotion.items():
     print(f"{e}: {score}")
 
 
+def main():
+    while True:
+        text = input("customer: ")
+        emotion = analyze_emotions(text)
+        for e, score in emotion.items():
+            print(f"{e}: {score}")
+        answeable = input("agent: ")
+        if answeable == "exit":
+            break
+
+
+main()
